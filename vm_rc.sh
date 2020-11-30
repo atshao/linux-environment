9bf4d6e1b132e8c7d12f2283d56941e7d3901e44
atshao <shao.tu@gmail.com>
2020-11-30 09:44:05 +0800

    Enhance script of virtual machines

--- vm_rc.sh
+++ vm_rc.sh
@@ -1,41 +1,40 @@
-if [ ${VBOX_INSTALLED} ]; then
+if [ $(which VBoxManage 2> /dev/null) ]; then
     alias vmup='VBoxManage startvm'
     alias lsvm='VBoxManage list vms'
-    alias lsvm-all='VBoxManage list vms'
-    alias lsvm-running='VBoxManage list runningvms'
 
     function vmdown() {
-        if [ "x${1}" = "x" ]; then
-            echo "ERROR: no virtual machine name specified."
-        else
+        if [ "x${1}" != "x" ]; then
             VBoxManage controlvm "${1}" poweroff
+        else
+            local num_vms
+            num_vms=$(VBoxManage list runningvms | wc -l)
+            if [ ${num_vms} -eq 0 ]; then
+                echo "INFO: no virtual machine running."
+            elif [ ${num_vms} -eq 1 ]; then
+                local vm
+                vm=$(VBoxManage list runningvms | cut -d'"' -f2)
+                VBoxManage controlvm "${vm}" poweroff
+            else
+                echo "ERROR: no virtual machine name specified."
+            fi
         fi
     }
-    function _vmup() {
-        local cur
-        COMPREPLY=()
+    function _vm() {
+        [ "${#COMP_WORDS[@]}" != "2" ] && return
 
-        cur="${COMP_WORDS[$COMP_CWORD]}"
-        case $COMP_CWORD in
-        1)  # available vms
+        local vms
+        if [ "${COMP_WORDS[0]}" == "vmup" ]; then
             vms=$(VBoxManage list vms | cut -d'"' -f2)
-            COMPREPLY=( $(compgen -W "${vms}" -- ${cur}) )
-            ;;
-        esac
-    }
-    function _vmdown() {
-        local cur
-        COMPREPLY=()
-
-        cur="${COMP_WORDS[$COMP_CWORD]}"
-        case $COMP_CWORD in
-        1)  # running vms
+        elif [ "${COMP_WORDS[0]}" == "vmdown" ]; then
             vms=$(VBoxManage list runningvms | cut -d'"' -f2)
-            COMPREPLY=( $(compgen -W "${vms}" -- ${cur}) )
-            ;;
-        esac
+        else
+            return
+        fi
+
+        COMPREPLY=($(compgen -W "${vms}" -- "${COMP_WORDS[1]}"))
     }
-    complete -F _vmup vmup
-    complete -F _vmdown vmdown
+    complete -F _vm lsvm
+    complete -F _vm vmup
+    complete -F _vm vmdown
 fi
 

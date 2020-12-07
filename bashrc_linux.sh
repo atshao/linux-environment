435aaa768618d76e7aac3a58cbd99c0d3d0c9ce8
atshao <shao.tu@gmail.com>
2020-12-07 13:36:45 +0800

    Regular commit

--- bashrc_linux.sh
+++ bashrc_linux.sh
@@ -1,16 +1,23 @@
 ###############################################################################
-[ -f /etc/bashrc ] && . /etc/bashrc
+# System defaults.
+###############################################################################
+[ -f "/etc/bashrc" ] && source "/etc/bashrc"
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
 ###############################################################################
-PATH="${HOME}/.alex/bin"
-PATH="${HOME}/.local/bin"
-PATH="${PATH}:/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
-export PATH
+# Custom environment variables regarding paths.
+###############################################################################
+# projects
+export DIR_PJ="${HOME}/Workspaces"
+
+# nvm (node)
+export DIR_NVM="${HOME}/.nvm"
+
+# virtual env
+export DIR_VV="${HOME}/.alex/venv"
 
 ###############################################################################
-MANPATH="${MANPATH}:"
-export MANPATH
-
+# NVM/Node behaviors.
 ###############################################################################
 # This loads nvm
 if [ -s "${DIR_NVM}/nvm.sh" ]; then
@@ -22,7 +29,10 @@ if [ -s "${DIR_NVM}/bash_completion" ]; then
     source "${DIR_NVM}/bash_completion"
 fi
 
-# Load our own resources
+###############################################################################
+# Load our own resources.
+###############################################################################
+# Custom rc files.
 if [ -d "${HOME}/.alex/etc" ]; then
     for ish in ${HOME}/.alex/etc/*_rc.sh; do
         source "${ish}"
@@ -30,18 +40,23 @@ if [ -d "${HOME}/.alex/etc" ]; then
 fi
 unset ish
 
-###############################################################################
+# Custom PATH
+PATH="${HOME}/.local/bin:${PATH}"
+PATH="${HOME}/.alex/bin:${PATH}"
+export PATH
+
+# Custom aliases
 alias r='fc -s'
 alias ls='/usr/bin/ls -F --color=auto --group-directories-first'
 alias la='/usr/bin/ls -aF --color=auto --group-directories-first'
 alias ll='/usr/bin/ls -lF --color=auto --group-directories-first'
 alias lla='/usr/bin/ls -laF --color=auto --group-directories-first'
 
-###############################################################################
+# Custom PS1
 export PS1="$(__alex_ps1)"
 
-###############################################################################
+# Custom umask
 umask 0002
 
-###############################################################################
+# Custom editor mode
 set -o vi
